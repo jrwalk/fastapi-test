04f56af5a8527b0ad0c6f818da18072c77172238
John Walk <johnrwalk@gmail.com>
2019-08-04 20:59:53 -0400

    enum input types

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.schema import User
+from app.schema import User, Metrics
 
 
 app = FastAPI()
@@ -15,3 +15,8 @@ def home():
 def age_user(user: User):
     user.age += 1
     return user
+
+
+@app.post("/similarity/{metric}")
+def similarity(metric: Metrics):
+    return {"selected_metric": metric}
