c664618aeee99fe4cd5f133b210f262cf667d77d
John Walk <johnrwalk@gmail.com>
2019-08-19 09:06:29 -0400

    black formatting

--- app/app.py
+++ app/app.py
@@ -8,25 +8,26 @@ from app.model import ModelWrapper
 app = FastAPI()
 model = ModelWrapper("/opt/fastapi-test/models/model.pkl")
 
+
 @app.get("/")
 async def home():
-    reply = ("this sentence is already halfway over,"
-            " and still hasn't said anything at all")
+    reply = (
+        "this sentence is already halfway over,"
+        " and still hasn't said anything at all"
+    )
     return {"message": reply}
 
 
-@app.post('/model/predict', response_model=Output, response_class=UJSONResponse)
+@app.post("/model/predict", response_model=Output, response_class=UJSONResponse)
 def predict(input: Input):
-    return {
-        "class_label": model.predict(input.dict())
-    }
+    return {"class_label": model.predict(input.dict())}
 
-@app.post("/model/score/{method}", response_model=ScoreOutput, response_class=UJSONResponse)
+
+@app.post(
+    "/model/score/{method}", response_model=ScoreOutput, response_class=UJSONResponse
+)
 def score(method: ValidScores, input: ScoreInput):
     data = input.data.dict()
     label = input.label
     method = method.value
-    return {
-        "score": model.score(data, label, method),
-        "method": method
-    }
+    return {"score": model.score(data, label, method), "method": method}
